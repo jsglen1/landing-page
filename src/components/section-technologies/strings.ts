8d50ce4adaf8deb6b11fe767b38f73f42ac82b5b
jsglen1 <wtfdxd@gmail.com>
2024-03-25 12:45:14 -0500

    add text content to section technologies

--- src/components/section-technologies/strings.ts
+++ src/components/section-technologies/strings.ts
@@ -12,84 +12,84 @@ export const CONTENT: ContentType[] = [
     id: 1,
     title: "Next",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "He trabajado con Next.js, desarrollando aplicaciones web dinámicas usando React. Tengo experiencia en enrutamiento dinámico, renderizado del lado del servidor y optimización de rendimiento.",
     url: "/nextjs.svg",
   },
   {
     id: 2,
     title: "React",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "He trabajado con React, desarrollando aplicaciones web dinámicas. Tengo experiencia en gestión de estado, componentes reutilizables, y optimización de rendimiento para una experiencia de usuario fluida.",
     url: "/react.svg",
   },
   {
     id: 3,
     title: "Node Express",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "He trabajado con Node.js y Express para crear servidores web robustos. Tengo experiencia en enrutamiento, middleware, manejo de solicitudes y respuestas, y desarrollo de API RESTful.",
     url: "/node.svg",
   },
   {
     id: 4,
     title: "Nest",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "He trabajado con Nest.js, desarrollando aplicaciones web y APIs con TypeScript. Tengo experiencia en el uso de decoradores, inyección de dependencias, enrutamiento, y la creación de módulos reutilizables.",
     url: "/nest.svg",
   },
   {
     id: 5,
     title: "Sql Server",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "He trabajado con SQL Server, gestionando bases de datos relacionales. Tengo experiencia en diseño de esquemas, consultas SQL, almacenamiento de procedimientos, y optimización de consultas para mejorar el rendimiento de la base de datos.",
     url: "/microsoft-sql-server.svg",
   },
   {
     id: 6,
     title: "My Sql",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "He trabajado con MySQL, administrando bases de datos relacionales. Tengo experiencia en diseño de esquemas, consultas SQL, almacenamiento de procedimientos y funciones, y optimización de consultas para mejorar el rendimiento de la base de datos.",
     url: "/mysql.svg",
   },
   {
     id: 7,
     title: "Postgres Sql",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "He trabajado con PostgreSQL, administrando bases de datos relacionales. Tengo experiencia en diseño de esquemas, consultas SQL avanzadas, almacenamiento de procedimientos y funciones, y optimización de consultas para mejorar el rendimiento de la base de datos.",
     url: "/postgresql.svg",
   },
   {
     id: 8,
     title: "MongoDB",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "He trabajado con MongoDB, administrando bases de datos NoSQL. Tengo experiencia en diseño de esquemas flexibles, consultas avanzadas utilizando el lenguaje de consulta de MongoDB (MongoDB Query Language), indexación eficiente y operaciones de agregación para análisis de datos.",
     url: "/mongodb.svg",
   },
   {
     id: 9,
     title: "Javascript",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "Tengo experiencia en JavaScript, desarrollando aplicaciones web dinámicas y funcionalidades interactivas. He trabajado en proyectos utilizando tanto el lado del cliente (front-end) como el lado del servidor (back-end) con Node.js.",
     url: "/javascript.svg",
   },
   {
     id: 10,
     title: "Typescript",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "He trabajado con TypeScript, desarrollando aplicaciones web y servicios backend. Tengo experiencia en la creación de tipos de datos personalizados, interfaces, y en el uso de características avanzadas como decoradores y tipos de uniones.",
     url: "/typescript.svg",
   },
   {
     id: 11,
     title: "Java",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "He trabajado con Java, desarrollando aplicaciones de backend y escritorio. Tengo experiencia en la programación orientada a objetos, desarrollo de API RESTful, manejo de bases de datos con JDBC y frameworks como Spring y Hibernate.",
     url: "/java.svg",
   },
   {
     id: 12,
     title: "Kotlin",
     content:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "He trabajado con Kotlin, desarrollando aplicaciones para Android, backend y aplicaciones multiplataforma. Tengo experiencia en programación funcional y orientada a objetos, desarrollo de aplicaciones Android, y construcción de servicios backend",
     url: "/kotlin.svg",
   },
 ];
